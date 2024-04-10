718dc4fc9cf15e00a60cf38003900aa1b91f4174
GitHub-Goutham <gparsi@asu.edu>
2024-04-10 16:56:59 -0700

    initital code template

---
+++ java file.java
@@ -0,0 +1,13 @@
+package asuHelloWorldJavaFX;
+import javafx.application.Application;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class ASUHelloWorldJavaFX extends Application {
+public static void main(String[] args) {
+launch(args);
+}
+}
